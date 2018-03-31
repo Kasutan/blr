8178e43a512b478bea98d4c86bd08f31a239f31b
Kasutan <contact@kasutan.pro>
2018-03-31 20:40:37 +0200

    Header sticky et popups chargées dans le footer

---
+++ themes/cael/template-parts/popup-acces.php
@@ -0,0 +1,8 @@
+<div class="reveal" id="acces-direct" data-reveal>
+  <h1>Popup acces direct</h1>
+  
+  <p>Icones des activites</p>
+  <button class="close-button" data-close aria-label="Close modal" type="button">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
---
+++ themes/cael/template-parts/popup-recherche.php
@@ -0,0 +1,8 @@
+<div class="reveal" id="recherche" data-reveal>
+  <h1>Popup recherche</h1>
+  
+  <?php get_search_form( true ); ?>
+  <button class="close-button" data-close aria-label="Close modal" type="button">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
