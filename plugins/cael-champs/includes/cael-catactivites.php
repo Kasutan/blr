9a2680b73ac67ea62d367f49a4d94480066e911f
RodolpheKasutan <rodolphe@kasutan.pro>
2018-04-16 12:28:27 +0200

    Mise à jour de l'activité de la page d'accueil

--- plugins/cael-champs/includes/cael-catactivites.php
+++ plugins/cael-champs/includes/cael-catactivites.php
@@ -47,20 +47,10 @@ add_action( 'cmb2_admin_init', function() {
 		'escape_cb'       => 'absint',
 	) );
 
-	// champs ajoutés dans les pages d'event du plugin eventon
-	$cmb_eventactivites = new_cmb2_box( array(
-		'id'            => 'evantmea',
-		'title'         => __( 'Mise en avant de l&acute;activité', 'cmb2' ),
-		'object_types' => array( 'ajde_events' ), // term data
-		'context'       => 'normal',
-		'priority'      => 'high',
-		'show_names'    => true, 
-		'closed'     => true,
-	) );
-
-	$cmb_eventactivites->add_field( array(
+	$cmb_catactivites->add_field( array(
 		'name'       => __( 'Mise en avant sur la page d&acute;accueil', 'cmb2' ),
 		'id'         => CMB_PREFIX . '_event_mea',
+		'desc' => __( 'pour les catégories de dernier niveau correspondant aux activités', 'msft-newscenter' ),
 		'type' => 'checkbox',	
 	) );
 
