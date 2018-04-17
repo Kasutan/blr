8ce0b238ee37abd1378cddaca209b4474a9fd45c
RodolpheKasutan <rodolphe@kasutan.pro>
2018-04-17 02:03:13 +0200

    Affichage de la page des activités

--- themes/cael/eventon/includes/activitesn3.php
+++ themes/cael/eventon/includes/activitesn3.php
@@ -1,11 +1,17 @@
 <?php 
+	setlocale(LC_ALL, 'fra');
 	$tax = get_query_var( 'taxonomy' );
 	$term_name = get_query_var( 'term' );
 	$term = get_term_by( 'slug', $term_name, $tax );
 	$ID = $term->term_id;
 	$imagelienid = get_term_meta( $ID, CMB_PREFIX.'_image_id', 1 );
 
-	$titre1  = get_post_meta( 7, CMB_PREFIX.'_Singleactivites_titre1', true );
+	$texte1  = get_post_meta( 7, CMB_PREFIX.'_Singleactivites_titre1', true );
+	$texte2  = get_post_meta( 7, CMB_PREFIX.'_Singleactivites_titre2', true );
+	$texte3  = get_post_meta( 7, CMB_PREFIX.'_Singleactivites_titre3', true );
+	$texte4  = get_post_meta( 7, CMB_PREFIX.'_Singleactivites_titre4', true );
+	$texte5  = get_post_meta( 7, CMB_PREFIX.'_Singleactivites_titre5', true );
+	$texte6  = get_post_meta( 7, CMB_PREFIX.'_Singleactivites_titre6', true );
 
 	$args = array(
 		'post_type' => 'ajde_events',
@@ -24,9 +30,10 @@
 			$query2->the_post();
 			$IDevent = get_the_id();
 			$organizer_terms = wp_get_post_terms($IDevent, 'event_organizer');
+			$location_terms = wp_get_post_terms($IDevent, 'event_location');
 			$testevent = get_post_custom($IDevent);
-	}
-	//var_dump($testevent);
+		}
+
 	?>
 
 	<header class="entry-header">
@@ -40,30 +47,49 @@
 		<h2>
         	<?php echo($term->name); ?>
     	</h2>
-		<h3>
-			<?php echo($titre1); ?>
-			<?php echo($organizer_terms[0]->name);?>
-			
-		</h3>
+		
+		<?php echo($texte1); ?>
+		<?php echo($organizer_terms[0]->name);?>
+		
 		<p>
 			<?php echo($term->description); ?>
 		</p>
 
+		<div class="grid-x">
+			<?php 	
+				if ( $query->have_posts() ) {
+					while($query->have_posts()): $query->the_post();
 
-		<?php 	
-			if ( $query->have_posts() ) {
-				while($query->have_posts()): $query->the_post();
+						$IDquery = get_the_ID();
+						$niveauevent = get_post_meta( $IDquery, 'evcal_subtitle', true );
+						$debut = get_post_meta( $IDquery, 'evcal_srow', true );
+						$fin = get_post_meta( $IDquery, 'evcal_erow', true );
 
-					$IDquery = get_the_ID();
-					$titre = get_the_title();
-					$lien= get_the_permalink();
-					$organizer_terms = wp_get_post_terms($IDquery, 'event_organizer');
-					echo('test');
-					echo($organizer_terms[0]->name);
+						?><div class"cell small-1 medium-2 large-3"><?php
+							echo($niveauevent);
+							echo date_i18n($format . 'l G\hi', $debut );
+							echo date_i18n($format . ' > G\hi', $fin );
+						?></div><?php
+					endwhile;
+				}
+				
+			?>
+		</div>
+		<div class"adresse">
+			<?php echo($texte2); ?>	
+			<br/>
+			<?php echo($texte3); echo(' '); echo($location_terms[0]->name);?>
+		</div>
 
-				endwhile;
-			}
-			wp_reset_postdata();
-		?>
-	
+		<div>
+		<?php echo($texte4); ?>	
+			<br/>
+			<?php echo($texte5); echo(' '); ?><a href=""><?php echo($texte6);?></a>
+		</div>
+
+	<?php echo get_tax_navigation( 'event_type', 'previous' ); ?>
+	<?php echo get_tax_navigation( 'event_type', 'next' ); ?>
+
+	<?php 
+	wp_reset_postdata();?>
 	</div><!-- .entry-content -->
