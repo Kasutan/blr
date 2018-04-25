5ccad8c066ec58b630bd73b6ede4942fc5b8a9e7
RodolpheKasutan <rodolphe@kasutan.pro>
2018-04-25 02:48:37 +0200

    Intégration menu ancres

--- themes/cael/modele-renseignement.php
+++ themes/cael/modele-renseignement.php
@@ -5,36 +5,25 @@ Template Name: Renseignements
 
 get_header();
 $ID=get_the_ID();
-/*
 ?>
 
-	<div id="primary" class="content-area lecael">
 		<main id="main" class="site-main">
 
 		<nav class = "ancres">
-			<a href="#association">
-				<?php echo wp_kses_post(get_post_meta( $ID, CMB_PREFIX.'_lecael_asso_titre1', true )); ?>
+			<a href="#contact">
+				<?php echo wp_kses_post(get_post_meta( $ID, CMB_PREFIX.'_rens_contact_titre1', true )); ?>
 			</a>
-			<a href="#projet">
-				<?php echo wp_kses_post(get_post_meta( $ID, CMB_PREFIX.'_lecael_projet_titre1', true )); ?>
+			<a href="#horaires">
+				<?php echo wp_kses_post(get_post_meta( $ID, CMB_PREFIX.'_rens_horaires_titre1', true )); ?>
 			</a>
-			<a href="#chiffres">
-				<?php echo wp_kses_post(get_post_meta( $ID, CMB_PREFIX.'_lecael_chiffres_titre1', true )); ?>
+			<a href="#acces">
+				<?php echo wp_kses_post(get_post_meta( $ID, CMB_PREFIX.'_rens_acces_titre1', true )); ?>
 			</a>
-			<a href="#equipe">
-				<?php echo wp_kses_post(get_post_meta( $ID, CMB_PREFIX.'_lecael_equipe_titre1', true )); ?> 
-			</a>
-			<a href="#administration">
-				<?php echo wp_kses_post(get_post_meta( $ID, CMB_PREFIX.'_lecael_equipe_titre21', true ));
-				echo (" ");
-				echo wp_kses_post(get_post_meta( $ID, CMB_PREFIX.'_lecael_equipe_titre22', true )); ?>
-			</a>
-			<a href="#partenaires">
-				<?php echo wp_kses_post(get_post_meta( $ID, CMB_PREFIX.'_lecael_part_titre1', true )); ?>
+			<a href="#inscriptions">
+				<?php echo wp_kses_post(get_post_meta( $ID, CMB_PREFIX.'_rens_inscriptions_titre1', true )); ?> 
 			</a>
 		</nav>
 
-		<?php */?>
 			<h1>
 				<?php the_title();?>
 			</h1>
@@ -47,7 +36,6 @@ $ID=get_the_ID();
 
 
 		</main><!-- #main -->
-	</div><!-- #primary -->
 
 <?php
 get_footer();
