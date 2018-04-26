9679a046a41473336a411d89d40dbf52deff38b2
Kasutan <contact@kasutan.pro>
2018-04-26 14:27:49 +0200

    CSS bandeau newsletter

--- themes/cael/template-parts/newsletter.php
+++ themes/cael/template-parts/newsletter.php
@@ -10,13 +10,13 @@ $validation = get_post_meta( 5, CMB_PREFIX.'_newsletter_validation', true);
 
 <section class="newsletter blanc fond-rose-clair">
 <form action="code mailchimp spécifique" method="post" id="cael-embedded-subscribe-form" name="cael-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
-    <div id="cael-embed-signup" class="grid-x align-center">
-			<label for="mce-EMAIL"><h3><?php  echo esc_html( $titre ); ?></h3><br><?php  echo esc_html( $soustitre ); ?></label>
-			<input value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="<?php  echo esc_html( $saisie ); ?>" required="" data-cip-id="à modifier" type="email" onclick="jQuery('#newsletter-info').show()">
+		<div id="cael-embed-signup" class="grid-x align-center align-middle">
+			<label for="mce-EMAIL" class="cell medium-6 large-3"><h3><?php  echo esc_html( $titre ); ?></h3><?php  echo esc_html( $soustitre ); ?></label>
+			<input value="" name="EMAIL" class="email cell medium-6 large-3" id="mce-EMAIL" placeholder="<?php  echo esc_html( $saisie ); ?>" required="" data-cip-id="à modifier" type="email" onclick="jQuery('#newsletter-info').show()">
 			
 			<div style="position: absolute; left: -5000px;" aria-hidden="true"><input name="modifier" tabindex="-1" value="" data-cip-id="modifier" type="text"></div>
-			<input value="<?php  echo esc_html( $bouton ); ?>" name="subscribe" id="cael-embedded-subscribe" class="button" onclick="if(!this.form.consent.checked){jQuery('#alerte').show();return false}" type="submit">
-			<div id="newsletter-info" class="consentement cell small-12" style="display:none">
+			<input value="<?php  echo esc_html( $bouton ); ?>" name="subscribe"  class="cell medium-6 large-2" id="cael-embedded-subscribe" class="button" onclick="if(!this.form.consent.checked){jQuery('#alerte').show();return false}" type="submit">
+			<div id="newsletter-info" class="consentement cell small-12 large-8" style="display:none">
 				<input id="consent" name="consent" value="yes" required type="checkbox">
 				<label for="consent"><?php  echo esc_html( $casecocher ); ?></label>
 				<p id="alerte" style="display:none"><small><?php  echo esc_html( $validation ); ?></small></p>
