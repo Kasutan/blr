f43e23aafb83e6f267df6383f60da9e3fec32446
RodolpheKasutan <rodolphe@kasutan.pro>
2018-04-27 02:17:27 +0200

    Ajout des icones et des liens

--- themes/cael/template-parts/popup-acces.php
+++ themes/cael/template-parts/popup-acces.php
@@ -1,3 +1,14 @@
+<?php
+$event_terms = get_terms(
+		'event_type',
+		array(
+			'orderby'=>'name',
+			'parent' => 0,
+			'hide_empty'=>false
+		)
+	);
+?>
+
 <div class="reveal" id="acces-direct" data-reveal>
   <p><span class="icon-bolt"></span></p>
   <p>
@@ -7,11 +18,30 @@
   </p>
   
   <div class="grid-x align-middle align-center">
-    <?php for($i=0;$i<6;$i++) :?>
-      <div class="bulle">&nbsp;</div>
-    <?php endfor; ?>
+    <?php 	
+    if(!empty($event_terms) && !is_wp_error($event_terms)){
+		  foreach($event_terms as $event_term){
+        $lien= get_term_link($event_term);
+        $titreevent=$event_term->name;
+			  $eventid=$event_term->term_id;
+			  $imagelien = get_term_meta( $eventid, CMB_PREFIX.'_image', 1 );
+        ?>
+
+        <a href= <?php echo ($lien); ?> class="lien">
+          <div class="bulle">
+		        <figure>
+				      <img src=<?php echo ($imagelien); ?> alt=<?php  echo esc_html( $titreevent ); ?>
+				      class="picto">
+			      </figure>
+          </div>
+        </a>
+		    <?php
+		  }
+	  };?>
   </div>
   <button class="close-button" data-close aria-label="Fermer la popup" type="button">
     <span aria-hidden="true">&times;</span>
   </button>
 </div>
+
+<?php wp_reset_postdata(); ?>
